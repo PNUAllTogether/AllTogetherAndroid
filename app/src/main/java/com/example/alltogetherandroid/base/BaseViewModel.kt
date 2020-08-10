c03ba821a29bee06af84167ef4dee873e3d4196f
최우영 <djdj7087@naver.com>
2020-08-10 22:56:34 +0900

    0810 BaseViewModel.kt change open

--- app/src/main/java/com/example/alltogetherandroid/base/BaseViewModel.kt
+++ app/src/main/java/com/example/alltogetherandroid/base/BaseViewModel.kt
@@ -3,7 +3,7 @@ package com.example.alltogetherandroid.base
 import androidx.lifecycle.ViewModel
 import io.reactivex.disposables.CompositeDisposable
 
-class BaseViewModel: ViewModel() {
+open class BaseViewModel: ViewModel() {
 
     private val compositeDisposable = CompositeDisposable()
 
