87b3399ad5347d26093bb332cd072fe676c4216b
Jaeyeong <201524443@pusan.ac.kr>
2020-08-11 23:13:05 +0900

    네이버 로그인 sdk 적용

--- app/src/main/java/com/alltogether/alltogetherandroid/base/BaseViewModel.kt
+++ app/src/main/java/com/alltogether/alltogetherandroid/base/BaseViewModel.kt
@@ -1,9 +1,16 @@
 package com.alltogether.alltogetherandroid.base
 
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 import com.alltogether.alltogetherandroid.utils.SingleLiveEvent
+import io.reactivex.Single
+import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.disposables.Disposable
+import io.reactivex.functions.Consumer
+import io.reactivex.schedulers.Schedulers
+import java.util.concurrent.TimeUnit
 
 open class BaseViewModel: ViewModel() {
     private val _startLoadingIndicatorEvent: SingleLiveEvent<Any> = SingleLiveEvent()
@@ -14,6 +21,25 @@ open class BaseViewModel: ViewModel() {
         get() = _stopLoadingIndicatorEvent
 
     private val compositeDisposable = CompositeDisposable()
+
+    fun addDisposable(disposable: Disposable) {
+        compositeDisposable.add(disposable)
+    }
+
+    fun <T> apiCall(single: Single<T>, onSuccess: Consumer<in T>,
+                    onError: Consumer<in Throwable> = Consumer {
+                        it.message?.let {msg ->
+                            Log.e("오류 발생", msg)
+                        }
+                    },
+                    indicator : Boolean = false, timeout: Long = 5){
+        addDisposable(single.subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .timeout(timeout, TimeUnit.SECONDS)
+            .doOnSubscribe{ if(indicator) startLoadingIndicator() }
+            .doAfterTerminate { stopLoadingIndicator() }
+            .subscribe(onSuccess, onError))
+    }
 
     override fun onCleared() {
         super.onCleared()
